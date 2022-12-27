cd9fd032862fb940b74fb3b0cdeabdc3c0e8416b
marcosd122 <marcos.adv.guimaraes@gmail.com>
2022-12-27 13:44:14 -0400

    commit inicial

---
+++ README.md
@@ -0,0 +1,17 @@
+# Atividade -- String para Float
+
+Projeto de desenvolvimento String para Float - Java
+
+Atividade curso Java
+
+## Tarefas
+
+O controle das tarefas desse projeto será realizado no Github
+
+## Ícones
+
+- :package: nova funcionalidade
+- :up: atualização
+- :bug: correção de bug
+- :checkered_flag: release
+
---
+++ src/string/para/pkgfloat/StringParaFloat.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package string.para.pkgfloat;
+
+/**
+ *
+ * @author Marcos
+ */
+
+public class StringParaFloat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        String str = "420.22";
+        
+        //primeiro metodo conversão
+        
+        float f = Float.valueOf(str);
+        
+        //segundo metodo conversão
+        //float fsecond = Float.parseFloat(str);
+        
+        System.out.println("\n convertendo o valor float :"+f);
+        
+    }
+    
+}
